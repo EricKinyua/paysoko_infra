0f71c68aaf3401a312ed5b94b6bf7e0ae88e81fb
EricKinyua <eriquekinyua0@gmail.com>
2025-05-07 23:22:34 +0300

    Init ansible

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ resource "digitalocean_droplet" "server" {
   ssh_keys = [digitalocean_ssh_key.default.id]
   tags     = ["dev"]
 
-  user_data = file("setup.sh")
+  # user_data = file("setup.sh")
 }
 # Create a PostgreSQL database cluster
 #Provision a managed database instance
