e04a91866494926ad57418b5c65d70224ea3a943
EricKinyua <eriquekinyua0@gmail.com>
2025-05-08 21:58:16 +0300

    Ansible-> Copy application files and change node app

--- node/app.js
+++ node/app.js
@@ -1,1 +1,12 @@
-console.log("Hello from Node.js");
+const http = require('http');
+
+// Use PORT env variable, fallback to 3000 if undefined
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.end(`Hello from Node App on port ${port}`);
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
