e407d9714e8d2fd595849ee3884ef45144c19099
EricKinyua <eriquekinyua0@gmail.com>
2025-05-12 21:21:26 +0300

    Remove unused variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,9 +10,4 @@ variable "ssh_public_key" {
   type        = string
 }
 
-variable "ssh_public_key_path" {
-  description = "Path to the SSH public key"
-  type        = string
-  default     = "~/.ssh/id_rsa_do.pub"
 
-}
