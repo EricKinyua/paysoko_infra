a42c1280449dde224a6b7ae17f0397e64824afa1
EricKinyua <eriquekinyua0@gmail.com>
2025-05-12 21:33:36 +0300

    Main config file

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,39 +7,39 @@ resource "digitalocean_ssh_key" "default" {
   public_key = var.ssh_public_key
 }
 
-# resource "digitalocean_droplet" "server" {
-#   count    = 5
-#   image    = "ubuntu-22-04-x64"
-#   name     = "server-${count.index + 1}"
-#   region   = "nyc2"
-#   size     = "s-1vcpu-1gb"
-#   ssh_keys = [digitalocean_ssh_key.default.id]
-#   tags     = ["dev"]
+resource "digitalocean_droplet" "server" {
+  count    = 5
+  image    = "ubuntu-22-04-x64"
+  name     = "server-${count.index + 1}"
+  region   = "nyc2"
+  size     = "s-1vcpu-1gb"
+  ssh_keys = [digitalocean_ssh_key.default.id]
+  tags     = ["dev"]
 
 
-# }
-# # Create a PostgreSQL database cluster
-# #Provision a managed database instance
-# resource "digitalocean_database_cluster" "mysql" {
-#   name       = "mysql-cluster"
-#   engine     = "mysql"
-#   version    = "8"
-#   size       = "db-s-1vcpu-1gb"
-#   region     = "nyc2"
-#   node_count = 1
-#   tags       = ["dev"]
+}
+# Create a PostgreSQL database cluster
+#Provision a managed database instance
+resource "digitalocean_database_cluster" "mysql" {
+  name       = "mysql-cluster"
+  engine     = "mysql"
+  version    = "8"
+  size       = "db-s-1vcpu-1gb"
+  region     = "nyc2"
+  node_count = 1
+  tags       = ["dev"]
 
-# }
+}
 
-# resource "digitalocean_database_db" "database" {
-#   cluster_id = digitalocean_database_cluster.mysql.id
-#   name       = "test"
-# }
+resource "digitalocean_database_db" "database" {
+  cluster_id = digitalocean_database_cluster.mysql.id
+  name       = "test"
+}
 
-# #create a cluster user
-# resource "digitalocean_database_user" "user" {
-#   cluster_id = digitalocean_database_cluster.mysql.id
-#   name       = "dev"
-# }
+#create a cluster user
+resource "digitalocean_database_user" "user" {
+  cluster_id = digitalocean_database_cluster.mysql.id
+  name       = "dev"
+}
 
 
