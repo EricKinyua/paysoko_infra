27b87280f96b9478589caaf0139024678607265f
EricKinyua <eriquekinyua0@gmail.com>
2025-05-13 21:57:12 +0300

    Destroy resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,61 +7,60 @@ resource "digitalocean_ssh_key" "default" {
   public_key = var.ssh_public_key
 }
 
-resource "digitalocean_droplet" "server" {
-  count    = 5
-  image    = "ubuntu-22-04-x64"
-  name     = "server-${count.index + 1}"
-  region   = "nyc2"
-  size     = "s-1vcpu-1gb"
-  ssh_keys = [digitalocean_ssh_key.default.id]
-  tags     = ["dev"]
+# resource "digitalocean_droplet" "server" {
+#   count    = 5
+#   image    = "ubuntu-22-04-x64"
+#   name     = "server-${count.index + 1}"
+#   region   = "nyc2"
+#   size     = "s-1vcpu-1gb"
+#   ssh_keys = [digitalocean_ssh_key.default.id]
+#   tags     = ["dev"]
 
 
-}
-resource "digitalocean_certificate" "cert" {
-  name = "certificate"
-  type = "lets_encrypt"
+# }
+# # resource "digitalocean_certificate" "cert" {
+# #   name = "certificate"
+# #   type = "lets_encrypt"
 
-}
-resource "digitalocean_loadbalancer" "public" {
-  name   = "loadbalancer"
-  region = "nyc2"
+# # }
+# resource "digitalocean_loadbalancer" "public" {
+#   name   = "loadbalancer"
+#   region = "nyc2"
 
-  forwarding_rule {
-    entry_port     = 443
-    entry_protocol = "https"
+#   forwarding_rule {
+#     entry_port     = 443
+#     entry_protocol = "https"
 
-    target_port     = 3001
-    target_protocol = "http"
+#     target_port     = 3001
+#     target_protocol = "http"
 
-    certificate_name = digitalocean_certificate.cert.name
-  }
+#   }
 
 
-  droplet_ids = digitalocean_droplet.server.*.id
-}
-# Create a PostgreSQL database cluster
-#Provision a managed database instance
-resource "digitalocean_database_cluster" "mysql" {
-  name       = "mysql-cluster"
-  engine     = "mysql"
-  version    = "8"
-  size       = "db-s-1vcpu-1gb"
-  region     = "nyc2"
-  node_count = 1
-  tags       = ["dev"]
+#   droplet_ids = digitalocean_droplet.server.*.id
+# }
+# # Create a PostgreSQL database cluster
+# #Provision a managed database instance
+# resource "digitalocean_database_cluster" "mysql" {
+#   name       = "mysql-cluster"
+#   engine     = "mysql"
+#   version    = "8"
+#   size       = "db-s-1vcpu-1gb"
+#   region     = "nyc2"
+#   node_count = 1
+#   tags       = ["dev"]
 
-}
+# }
 
-resource "digitalocean_database_db" "database" {
-  cluster_id = digitalocean_database_cluster.mysql.id
-  name       = "test"
-}
+# resource "digitalocean_database_db" "database" {
+#   cluster_id = digitalocean_database_cluster.mysql.id
+#   name       = "test"
+# }
 
-#create a cluster user
-resource "digitalocean_database_user" "user" {
-  cluster_id = digitalocean_database_cluster.mysql.id
-  name       = "dev"
-}
+# #create a cluster user
+# resource "digitalocean_database_user" "user" {
+#   cluster_id = digitalocean_database_cluster.mysql.id
+#   name       = "dev"
+# }
 
 
